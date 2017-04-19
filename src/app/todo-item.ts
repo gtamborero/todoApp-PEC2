6a33406515fe8ca5c19d73ad67d9ba6c0eced1ed
gtamborero <gtamborero@iproject.cat>
2017-04-20 00:47:42 +0200

    items con delete y add

--- src/app/todo-item.ts
+++ src/app/todo-item.ts
@@ -7,18 +7,36 @@ export class TodoItem {
 
 var TODOITEMS: TodoItem[] = [
   { id: 1, todoTxt: 'comprar pan' },
-  { id: 2, todoTxt: 'comprar galletas'}
+  { id: 2, todoTxt: 'comprar galletas'},
+  { id: 3, todoTxt: 'comprar turrón'}
 ];
 
 @Component({
   selector: 'todo-item',
   template: `
   <ul >
-  <li *ngFor="let item of todoItems">id: {{item.id}} {{item.todoTxt}} <a>X</a></li>
+  <li *ngFor="let item of todoItems">id: {{item.id}} {{item.todoTxt}} <a (click)=delete(item.id)>X</a></li>
   </ul>
   `
 })
 
 export class TodoItemComponent {
  todoItems = TODOITEMS;
+
+ constructor(){
+   this.add("goo");
+ }
+
+ delete(id){
+   // busco dentro del array el id X y cojo su índice
+   var index = this.todoItems.findIndex( (res) => res.id == id )
+   // Elimino el elemento
+   this.todoItems.splice(index, 1);
+ }
+
+ add(text){
+   // añado elemento al inicio (0), no borro ninguno,
+   // pendiente id autoincremental
+   this.todoItems.splice(0, 0, { id: 10, todoTxt: text });
+ }
 }
